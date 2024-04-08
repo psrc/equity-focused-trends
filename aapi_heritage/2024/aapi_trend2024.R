4bd4406f2faf01e9d94a6808d39fff998d94a3be
Joanne <71201494+Joanneylin@users.noreply.github.com>
2024-04-08 10:47:14 -0700

    add income figures

--- aapi_heritage/2024/aapi_trend2024.R
+++ aapi_heritage/2024/aapi_trend2024.R
@@ -3,7 +3,7 @@ library(psrcplot)
 library(tidyverse)
 
 # install psrccensus and get api key by going trough instructions on: https://psrc.github.io/psrccensus/articles/psrccensus.html
-Sys.getenv("f888d08b4cf518d8a5f7d4eb1fa050abb015ba37")
+Sys.getenv("CENSUS_API_KEY")
 
 # more information on PUMS data: https://www.census.gov/programs-surveys/acs/microdata/documentation.html
 # 2022 5-year PUMS data dictionary: https://api.census.gov/data/2022/acs/acs5/pums/variables.html
@@ -31,19 +31,19 @@ Sys.getenv("f888d08b4cf518d8a5f7d4eb1fa050abb015ba37")
 
 # download 2022 5-year PUMS data with specified variables
 pums_2022_h <- get_psrc_pums(span = 5,
-                           dyear = 2022,
-                           level = "h",
-                           vars = c("AGEP",  # Age
-                                    "PRACE", # Race
-                                    "RAC1P", # Recoded detailed race code
-                                    "RAC2P", # Recoded detailed race code
-                                    "TEN",   # Tenure
-                                    "GRPIP", # Gross rent as a percentage of household income past 12 months
-                                    "HINCP",  # Household income
-                                    "BIN_POVRATIO"#,
-                                    # "SOCP3",
-                                    # "SOCP5"
-                                    )) 
+                             dyear = 2022,
+                             level = "h",
+                             vars = c("AGEP",  # Age
+                                      "PRACE", # Race
+                                      "RAC1P", # Recoded detailed race code
+                                      "RAC2P", # Recoded detailed race code
+                                      "TEN",   # Tenure
+                                      "GRPIP", # Gross rent as a percentage of household income past 12 months
+                                      "HINCP",  # Household income
+                                      "BIN_POVRATIO"#,
+                                      # "SOCP3",
+                                      # "SOCP5"
+                             )) 
 ## ----- 1. households ----- 
 df_pums <- pums_2022_h %>%
   # make new variables
@@ -85,7 +85,7 @@ asian_top10 <- df_pums %>%
   filter(!RAC2P %in% c("All combinations of Asian races only","Other Asian alone","Total")) %>%
   arrange(desc(count)) %>%
   top_n(10,count)
-  
+
 
 ## ----- 3. AAPI households (householder) ----- 
 df_pums_aapi <- df_pums %>%
@@ -118,20 +118,21 @@ race_allpersons <- pums_2022_p[['variables']] %>%
   ungroup() %>%
   # at least one of AAPI member in household
   mutate(PRACE_allpersons = case_when(# both Asian and PI race in household
-                                      n_prace>1~"Multiple AAPI races",
-                                      # at least one AAPI member in household
-                                      all_prace=="Asian alone"~ "Asian",
-                                      all_prace=="Native Hawaiian and Other Pacific Islander alone"~ "Native Hawaiian and Other Pacific Islander"),
-         RAC2P_allpersons = case_when(# both Asian and PI race in household
-                                      n_prace>1~"Multiple AAPI races", 
-                                      # multiple asian subgroups in household
-                                      all_prace == "Asian alone" & n_rac2p>1~"Multiple Asian subgroups", 
-                                      # multiple PI subgroups in household
-                                      all_prace == "Native Hawaiian and Other Pacific Islander alone" & n_rac2p>1~"Multiple Native Hawaiian and Other Pacific Islander subgroups",
-                                      # at least one AAPI member in household
-                                      all_prace=="Asian alone"~ all_rac2p,
-                                      all_prace=="Native Hawaiian and Other Pacific Islander alone"~ all_rac2p)) %>%
+    n_prace>1~"Multiple AAPI races",
+    # at least one AAPI member in household
+    all_prace=="Asian alone"~ "Asian",
+    all_prace=="Native Hawaiian and Other Pacific Islander alone"~ "Native Hawaiian and Other Pacific Islander"),
+    RAC2P_allpersons = case_when(# both Asian and PI race in household
+      n_prace>1~"Multiple AAPI races", 
+      # multiple asian subgroups in household
+      all_prace == "Asian alone" & n_rac2p>1~"Multiple Asian subgroups", 
+      # multiple PI subgroups in household
+      all_prace == "Native Hawaiian and Other Pacific Islander alone" & n_rac2p>1~"Multiple Native Hawaiian and Other Pacific Islander subgroups",
+      # at least one AAPI member in household
+      all_prace=="Asian alone"~ all_rac2p,
+      all_prace=="Native Hawaiian and Other Pacific Islander alone"~ all_rac2p)) %>%
   select(SERIALNO,PRACE_allpersons,RAC2P_allpersons)
+
 
 df_pums_aapi_allpersons <- df_pums %>%
   filter(SERIALNO %in% race_allpersons$SERIALNO)
@@ -142,7 +143,9 @@ df_pums_aapi_allpersons[['variables']] <- df_pums_aapi_allpersons[['variables']]
     RAC2P_allpersons_aapi_group10 = case_when(PRACE_allpersons == "Asian" & RAC2P_allpersons %in% asian_top10$RAC2P~ RAC2P_allpersons,
                                               PRACE_allpersons == "Asian"~ "Other Asian subgroups",
                                               TRUE~PRACE_allpersons
-                                              ))
+    ))
+
+
 
 # ---- 6. AAPI persons data for occupation ----
 # all adults in AAPI households
@@ -171,9 +174,7 @@ income <- psrc_pums_median(df_pums_renter_aapi, stat_var = "HINCP", group_vars=c
 
 # job share for each AAPI subgroup
 job3_region <- pums_2022_p %>% filter(AGEP >= 15, !is.na(SOCP3)) %>%
-  psrc_pums_count(., group_vars=c("SOCP3")) %>%
-  arrange(desc(share)) %>%
-  top_n(5, share)
+  psrc_pums_count(., group_vars=c("SOCP3"))
 job3_by_aapi_race <- psrc_pums_count(df_pums_p_aapi_worker, group_vars=c("RAC2P_aapi_group10_houshold","SOCP3"))
 
 # top 5 occupations of each AAPI subgroup
